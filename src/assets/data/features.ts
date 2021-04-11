1fd1c17840e471dcbfc2d44f96f22a2cfcb2d82b
andrioc <andrio.rodrigo.silva@hotmail.com>
2021-04-11 16:45:33 -0300

    Createdata for box info

---
+++ src/assets/data/features.ts
@@ -0,0 +1,25 @@
+const features = [
+    {
+        id: 1,
+        title: 'Bamboo Stand',
+        price: 25,
+        description: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you'll be added to a special Backer member list.",
+        qtd: 101,
+    },
+    {
+        id: 2,
+        title: 'Black Edition Stand',
+        price: 75,
+        description: "You get a Black Special Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
+        qtd: 64,
+    },
+    {
+        id: 3,
+        title: 'Mahogany Special Edition',
+        price: 200,
+        description: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll be added to our Backer member list. Shipping is included.",
+        qtd: 0,
+    }
+]
+
+export default features
